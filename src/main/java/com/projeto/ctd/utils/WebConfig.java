924e7998763cbd7a1e9e5d6d0a4fada62dfdc487
Rener Almeida <rener.almeida@gmail.com>
2021-12-08 02:21:03 -0300

    testing

--- src/main/java/com/projeto/ctd/utils/WebConfig.java
+++ src/main/java/com/projeto/ctd/utils/WebConfig.java
@@ -1,6 +1,10 @@
 package com.projeto.ctd.utils;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,5 +20,19 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+
+    @Bean
+    public WebSecurityConfigurerAdapter webSecurity() {
+        return new WebSecurityConfigurerAdapter() {
+
+            @Override
+            protected void configure(HttpSecurity http) throws Exception {
+                http.headers().addHeaderWriter(
+                        new StaticHeadersWriter("Access-Control-Allow-Origin", "*"));
+
+
+            }
+        };
     }
 }
